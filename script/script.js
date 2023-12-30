2c133f080f16b2889ae95c1acee937990cd15316
Alix Labonne <labonne.alix@gmail.com>
2023-12-30 13:24:29 +0100

    format code with prettier

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll("#imageContainer img");
-    const imageLink = document.getElementById("imageLink");
-    let currentIndex = 0;
-  
-    function showImage(index) {
-      images.forEach((image, i) => {
-        image.classList.toggle("active", i === index);
-      });
-    }
-  
-    function rotateImages() {
-      currentIndex = (currentIndex + 1) % images.length;
-      showImage(currentIndex);
-    }
-  
-    function navigateToCollection() {
-      window.location.href = "collection.html";
-    }
-  
-    setInterval(rotateImages, 1000); // Rotate every 1 second
-  
-    imageLink.addEventListener("click", navigateToCollection);
-  });
+  const images = document.querySelectorAll("#imageContainer img");
+  const imageLink = document.getElementById("imageLink");
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((image, i) => {
+      image.classList.toggle("active", i === index);
+    });
+  }
+
+  function rotateImages() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  function navigateToCollection() {
+    window.location.href = "collection.html";
+  }
+
+  setInterval(rotateImages, 1000); // Rotate every 1 second
+
+  imageLink.addEventListener("click", navigateToCollection);
+});
